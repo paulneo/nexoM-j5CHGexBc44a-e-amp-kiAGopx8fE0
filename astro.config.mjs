094d1c6f5ad3f697b70df074433d823a36af7454
paulneo <jpaulneo2k@gmail.com>
2024-06-12 21:02:28 -0500

    change

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nexomundial.com/",
+  site: "https://www.nexomundial.com/",
   base: "/",
   trailingSlash: "always",
   integrations: [
